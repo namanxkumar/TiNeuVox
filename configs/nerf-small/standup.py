f5f3e17f109d8d238bca97708ca8cd541d8f60d9
Naman Kumar <namank2002@gmail.com>
2023-09-07 12:08:11 +0530

    Update standup.py

--- configs/nerf-small/standup.py
+++ configs/nerf-small/standup.py
@@ -1,10 +1,10 @@
 _base_ = './default.py'
 
 expname = 'small/dnerf_standup-400'
-basedir = './logs/nerf_synthetic'
+basedir = '/content/drive/My Drive/Colab Notebooks/TiNeuVox/logs/nerf_synthetic'
 
 data = dict(
-    datadir='/content/drive/My Drive/Colab Notebooks/data_dnerf/standup',
+    datadir='/content/drive/My Drive/Colab Notebooks/TiNeuVox/data_dnerf/standup',
     dataset_type='dnerf',
     white_bkgd=True,
 )
